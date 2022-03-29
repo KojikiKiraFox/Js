1eaa6906f7e97e4b4de0211d2b9ea15747c75732
KiraFox <kojikikirafox@gmail.com>
2022-03-29 15:13:41 +0300

    2

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let btn = document.querySelector('#btn')
 let text = document.querySelector('#text')
+let range = document.querySelector('#range')
+let circle = document.querySelector('#circle')
+let rangeSpan = document.querySelector('#range-span')
+let e_btn = document.querySelector('#e_btn')
+
+function start() {
+    rangeSpan.textContent = 50
+    circle.style.height = range.value + '%'
+    circle.style.width = range.value + '%'
+    e_btn.style.display = 'none'
+}
+
 
 const color = function() {
     btn.style.backgroundColor = text.value
 }
 
-btn.addEventListener('click', color)
-
-// 
-
-let e_btn = document.querySelector('#e_btn')
-e_btn.style.display = 'none'
-
-// 
-
-let range = document.querySelector('#range')
-let circle = document.querySelector('#circle')
-let rangeSpan = document.querySelector('#range-span')
 
 const position = function(){
     circle.style.height = range.value + '%'
@@ -24,5 +24,6 @@ const position = function(){
     rangeSpan.textContent = event.target.value
 }
 
+start()
+btn.addEventListener('click', color)
 range.addEventListener('input', position)
-console.log(circle);
