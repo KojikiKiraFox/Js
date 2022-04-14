ce27ea0a0d9d820472f5737227894f111e30054b
KiraFox <kojikikirafox@gmail.com>
2022-04-14 13:21:46 +0300

    2

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const one = () =>{
+    let timerHours = document.querySelector('#timer-hours')
+    let timerMinutes = document.querySelector('#timer-minutes')
+    let timerSeconds = document.querySelector('#timer-seconds')
+
+    console.log(timerHours);
+    console.log(timerMinutes);
+    console.log(timerSeconds);
+}
